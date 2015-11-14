77c6e6453f988d28bf4299c8e40b12719fd54eca
suzueri <suzueli@gmail.com>
2015-11-14 22:43:48 +0900

    add delete button

--- sketch_synth1.ino
+++ sketch_synth1.ino
@@ -15,6 +15,7 @@ const int buttonPin6 = 6;
 const int buttonPin7 = 7;
 const int buttonPin8 = 8; 
 const int buttonPin10 = 10; 
+const int buttonPin11 = 11; 
 
 int currentButtonState2 = 0;  
 int currentButtonState3 = 0;
@@ -24,6 +25,7 @@ int currentButtonState6 = 0;
 int currentButtonState7 = 0;
 int currentButtonState8 = 0;
 int currentButtonState10 = 0;
+int currentButtonState11 = 0;
 
 int formerButtonState2  = LOW;  
 int formerButtonState3  = LOW;
@@ -33,6 +35,7 @@ int formerButtonState6 = HIGH;
 int formerButtonState7 = HIGH;
 int formerButtonState8 = HIGH;
 int formerButtonState10 = HIGH;
+int formerButtonState11 = HIGH;
 
 bool button2On = false;
 bool button3On = false;
@@ -42,6 +45,7 @@ bool button6On = false;
 bool button7On = false;
 bool button8On = false;
 bool button10On = false;
+bool button11On = false;
 
 // シーケンサーのデータ
 enum {
@@ -50,7 +54,7 @@ enum {
 };
 
 uint8_t sequenceData[NUMBER_OF_STEPS][NUMBER_OF_VOICE] = {
- {1, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0},
@@ -99,7 +103,7 @@ void timer(){
   }
   
   const int volume = mozziAnalogRead(1);
-  int tempo = 1023 - volume; // ボリュームを時計回りに回すと早くしたいので、値を逆にします。
+  int tempo = 1023 - volume; // ボリュームを時計回りに回すと早くしたいので、値を逆に。
   if (tempo < 40) { // 速すぎ防止
     tempo = 40;
   }  
@@ -111,7 +115,6 @@ void timer(){
 void clearCurrentStep()
 {
   uint8_t* const currentSequence = sequenceData[currentStep];
-  
   for (int i = 0; i < NUMBER_OF_VOICE; ++i) {
     currentSequence[i] = 0;
   }
@@ -131,6 +134,7 @@ void setup(){
    pinMode(buttonPin7, INPUT_PULLUP);  
    pinMode(buttonPin8, INPUT_PULLUP);  
    pinMode(buttonPin10, INPUT_PULLUP);  
+   pinMode(buttonPin11, INPUT_PULLUP);  
    Serial.begin(115200);
   
 }
@@ -144,6 +148,7 @@ void updateControl(){
    formerButtonState7 = currentButtonState7;
    formerButtonState8 = currentButtonState8;
    formerButtonState10 = currentButtonState10;
+   formerButtonState11 = currentButtonState11;
    
    currentButtonState2 = digitalRead(buttonPin2);
    currentButtonState3 = digitalRead(buttonPin3);
@@ -153,6 +158,7 @@ void updateControl(){
    currentButtonState7 = digitalRead(buttonPin7);
    currentButtonState8 = digitalRead(buttonPin8);
    currentButtonState10 = digitalRead(buttonPin10);
+   currentButtonState11 = digitalRead(buttonPin11);
 
    //チャタリング防止かつ配列に
    if (currentButtonState2 == formerButtonState2
@@ -195,9 +201,7 @@ void updateControl(){
     } else {
       button5On = false;
     }
-    
-    
-    
+
     if (currentButtonState6 == formerButtonState6
      && currentButtonState6 == LOW) {
        button6On = true;
@@ -207,8 +211,6 @@ void updateControl(){
     } else {
       button6On = false;
     }
-    
-    
     
    if (currentButtonState7 == formerButtonState7
      && currentButtonState7 == LOW) {
@@ -239,9 +241,16 @@ void updateControl(){
     } else {
       button10On = false;
     }
-    
+   
+     if (currentButtonState11 == formerButtonState11
+     && currentButtonState11 == LOW) {
+       button11On = true;
+       clearCurrentStep();
 
-    
+    } else {
+      button11On = false;
+    }
+
 }
 
 int updateAudio()
